f6f822a73e4a22bb2beb8dc9ef5371f3f7a2e0dc
ArpitST <arpit@securitytroops.com>
2020-11-05 18:06:07 +0530

    seventh commit

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ $(document).ready(function(){
 		$("main").append('<section><h1>' +heading + '</h1></section>');
 		$(".select1 option").remove()
 		$(".select1").append('<option value="select">select</option>' )
+		$(".select4 option").remove()
+		$(".select4").append('<option value="select">select</option>' )
 		$('main section h1').each(function(index,value){
 			index=index+1;
 			var head = $(this).text()
 			$(".select1").append('<option value="'+index+'">' +head + '</option>');
+			$(".select4").append('<option value="'+index+'">' +head + '</option>');
 		})
 	});
 
@@ -16,19 +19,27 @@ $(document).ready(function(){
 		e.preventDefault();
 		var subheading = $('.head2').val();
 		var headingindex = $('.select1').val();
-		console.log(subheading);
-		console.log(headingindex);
 		$("main section:nth-child("+headingindex+")").append('<div><h2> ' +subheading + '</h2></div>');
-		$(".select2").append('<option>' +subheading + '</option>');
+		// $(".select2").append('<option>' +subheading + '</option>');
 	});
 
 // till above is correct
 
-	$(".btn3").click(function(e){
-		e.preventDefault();
-		var heading=$('.head3').val();
-		$("main section").append('<h3> ' +heading + '</h3>');
-		$(".select3").append('<option>' +heading + '</option>');
+	$(".select4").change(function(e){
+		var headingindex = $('.select4').val();
+		console.log(headingindex)
+		var hhh = headingindex-1;
+		console.log(hhh)
+
+		$(".select2 option").remove()
+		$(".select2").append('<option value="select">Select Sub Heading</option>' )
+		$('main section:nth-child('+headingindex+') div h2').each(function(index2,value2){
+			index2=index2+1;
+			var subhead = $(this).text()
+			$('.select2').append('<option value="'+index2+'">' +subhead + '</option>')
+		});
+		// $("main section").append('<h3> ' +heading + '</h3>');
+		// $(".select3 option").append('<option>' +heading + '</option>');
 	});
     
 	// to check button value select
