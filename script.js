87869b5e46e7487b4d27072ea76a9323a2d2efa5
ArpitST <arpit@securitytroops.com>
2020-11-12 11:00:03 +0530

    eleven commit

--- script.js
+++ script.js
@@ -24,12 +24,11 @@ $(document).ready(function(){
 	});
 	
 
-// for form section starts from here
+// form section starts from here
 
 	$(".select4").change(function(e){
 		var headingindex = $('.select4').val();
 		var hhh = headingindex-1;
-
 		$(".select2 option").remove()
 		$(".select2").append('<option value="select">Select Sub Heading</option>' )
 		$('main section:nth-child('+headingindex+') div h2').each(function(index2,value2){
@@ -39,94 +38,110 @@ $(document).ready(function(){
 		});
 	});
 
-    $('.formfh').submit(function(e){
-    	e.preventDefault()
-    	var hjk=$(".frmhdg select").val();
-    	var hkj=$(".sbhdg select").val();
-    	var hhh = parseInt(hkj)+1;
-    	var fminin=$('.select3 ').val();
-    	var lbbl=$('.lable').val();
-    	var naaame=$('.naaaam').val();
-    	var placcehol=$('.plachol').val();
-    	var claase=$('.clase').val();
-    	var vaalue=$('.vl').val();
-    	var opption=$('.opti').val();
-    	var disaablled=false;
-    	var readonlyy=false;
-    	var reeqquired=false; 
+  $('.formfh').submit(function(e){
+    e.preventDefault()
+    var hjk=$(".frmhdg select").val();
+    var hhh=$(".sbhdg select").val();
+    var hhh = parseInt(hhh)+1;
+    var fminin=$('.select3 ').val();
+    var lbbl=$('.lable').val();
+    var naaame=$('.naaaam').val();
+    var placcehol=$('.plachol').val();
+    var claase=$('.clase').val();
+    var vaalue=$('.vl').val();
+    var opption=$('.opti').val();
+    var disaablled=false;
+    var readonlyy=false;
+    var reeqquired=false; 
 
-    	if (fminin=='checkbox'){
-    		var opn=opption.split(',');
-    		console.log(opn)
-    		// var aws=$('<p></p>');
-    		$(opn).each(function(key, fucntionvalue){
-    			console.log(fucntionvalue)
-    			if(vaalue==fucntionvalue){
-    				$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+fucntionvalue+'</label><input type= '+fminin+' value='+vaalue+' class= '+claase+' name='+naaame+' selected="selected"><button onclick="MYfunction(this)">X</button></p>');
-    			}
-    			else{
-    				$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+fucntionvalue+'</label><input type= '+fminin+' value='+vaalue+' class= '+claase+' name='+naaame+'><button onclick="MYfunction(this)">X</button></p>');
-    			}
-    		});
-    	}	
+
+    if(fminin=='checkbox'){
+      var opn=opption.split(',');
+      console.log(opn)
+      var aws=$('<p></p>');
+      $(opn).each(function(key, fucntionvalue){
+    		console.log(fucntionvalue)
+    		if(vaalue==fucntionvalue){
+    			$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+fucntionvalue+'</label><input type= '+fminin+' value='+vaalue+' class= '+claase+' name='+naaame+' selected="selected"><button onclick="MYfunction(this)">X</button></p>');
+    		}
+    		else{
+    			$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+fucntionvalue+'</label><input type= '+fminin+' value='+vaalue+' class= '+claase+' name='+naaame+'><button onclick="MYfunction(this)">X</button></p>');
+    		}
+    	});
+    }	
     	
-    	else if(fminin=='radio'){
-    		var opn=opption.split(',');
-    		console.log(opn)
-    		// var fr=$('<p></p>');
-    		$(opn).each(function(key,fucntionvalue){
-    			console.log(fucntionvalue)
-    			if(vaalue==fucntionvalue){
-    				$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+fucntionvalue+'</label><input type="'+fminin+'" value="'+vaalue+'" class= '+claase+' name="'+naaame+'"checked="checked"><button onclick=MYfunction(this)">o</button></p>');
-    			}
-    			else{
-    				$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+fucntionvalue+'</label><input type= '+fminin+' value='+vaalue+' class='+claase+' name='+naaame+'><button onclick=MYfunction(this)">o</button></p>');
-    			}
-    		});
-    	}
+    else if(fminin=='radio'){
+    	var opn=opption.split(',');
+      console.log(opn)
+      var fr=$('<p></p>');
+      $(opn).each(function(key,sval){
+        if(vaalue==sval){
+          $('main section:nth-child('+hjk+') div:nth-child('+hhh+') ').append('<label>'+opn[key]+'</label><input type="'+fminin+'" class = '+claase+' value="'+vaalue+'" name="'+naaame+'"  checked="checked"><button onclick="MYfunction(this)">X</button>')
+        }
+        else{
+          $('main section:nth-child('+hjk+') div:nth-child('+hhh+') ').append('<label>'+opn[key]+'</label><input type="'+fminin+'" class = '+claase+' value="'+vaalue+'" name="'+naaame+'"><button onclick="MYfunction(this)">X</button>');
+        }
+      });
+    }
+    else if(fminin=='select'){
+      // $('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+lbbl+'</label><input type='+fminin+' name='+naaame+' placeholder='+placcehol+'  class='+claase+' value='+vaalue+'><button onclick="MYfunction(this)">X</button></p>');
+      var opns=opn.split(',');
+      console.log(opn)
+      var aws=$('<p><label>'+lbbl+'</label></p>')
+      var ae=$('<select class='+claase+' name='+naaame+'><option>select</option></select>').appendTo(aws)
+      var as=$('<button onclick="MYfunction(this)">X<button>').appendTo(aws);
+      for(i=0; i<opns.length;i++){
+        if(vaalue==opns[i]){
+          $(ae).append('<option value="'+opns[i]+'" selected="selected">' +opns[i]+'</option>')
+        }else
+        {
+          $(ae).append('<option value="'+opns[i]+'" selected="selected">' +opns[i]+'</option>')
+        }
+      }
+    }
 
-    	else if(fminin=='textarea'){
-    		$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+lbbl+'</label><input name='+naaame+' placeholder='+placcehol+' rows="4" cols="10" value='+vaalue+'><button>X</button></p>');
-    	}
-    	// till above is correct
-    });
+    else if(fminin=='textarea'){
+    	$('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+lbbl+'</label><input name='+naaame+' placeholder='+placcehol+' rows="4" cols="10" value='+vaalue+'><button>X</button></p>');
+    }
+    else {
+      $('main section:nth-child('+hjk+') div:nth-child('+hhh+')').append('<p><label>'+lbbl+'</label><input type='+fminin+' name='+naaame+' placeholder='+placcehol+'  class='+claase+' value='+vaalue+'><button onclick="MYfunction(this)">X</button></p>');
+    }
+    
+    if($(".disaablled").is(':checked')){
+      var sss=fminin
+      if(fminin=='number' || fminin=='email' || fminin=='button' || fminin=='text' || fminin=='checkbox' || fminin=='file' || fminin=='radio')
+      {
+        sss='input'
+      }
+      $('main section:nth-child('+hjk+') div:nth-child('+hhh+') p:last-child '+sss).attr('disaablled','disaablled');
+    }
 
-  //   	else if(fminin=='select'){
-  //   		var optiion=opn.split(',');
-  //   		var aws=$('<p><label>'+lbl+'</label></p>');
-  //   		var ae=$('<select claase='+cls+'name='+nm+'><option>select</option></select>').appendTo(aws);
-  //   		for (i=0; i<opns.length; i++){
-  //   			if(vaaalue==opns[i]){
-  //   				$(ae).append('<option value="'+opns[i]+'" selected=" selected">'+opns[i]+'</option>')
-  //   			}
-  //   			else{
-  //   				$(ae).append('<option value="'+opns[i]+'">'+opns[i]+'</option>')
-  //   			}
-  //   		}
-  //   	}
+    if($(".readonlyy").is(':checked')){
+      var sss=fminin
+      if(fminin=='number' || fminin=='email' || fminin=='button' || fminin=='text' || fminin=='checkbox' || fminin=='file' || fminin=='radio')
+      {
+        sss='input'
+      }
+      $('main section:nth-child('+hjk+') div:nth-child('+hhh+') p:last-child '+sss).attr('readonlyy','readonlyy');
+    }
 
-  //   	if ($(".disable").is(':checked')){
-		// 	var sss = fminin
-		// 	if (fminin == 'number' || fminin == 'email' || fminin == 'button' || fminin == 'text' || fminin == 'checkbox' || fminin == 'file' || fminin == 'radio')
-		// 	{
-		// 		sss = 'input'
-		// 	}
-		// 	$('main section:nth-child('+fsh+') div:nth-child('+fss+')  p:last-child '+sss).attr('disabled', 'disabled');	
-		//     [fsh-1].subheading[fss-2].form.push({'dis':true})	
-		// }
+    if ($(".reeqquired").is(':checked')) {
+      var sss=fminin
+      if(fminin=='number' || fminin=='email' || fminin=='button' || fminin=='text' || fminin=='checkbox' || fminin=='file' || fminin=='radio')
+      {
+        sss='input'
+      }
+      $('main section:nth-child('+hjk+') div:nth-child('+hhh+') p:last-child '+sss).attr('reeqquired','reeqquired');
+    }
+  });
 
-		// if ($(".readonly").is(':checked')){
-		// 	var sss=fminin
-		// 	if (fminin == 'email' || fminin == 'number' || fminin == 'text' || fminin == 'button' || fminin == 'checkbox' || fminin == 'file' || fminin == 'radio'){
-		// 		ddd = 'input'
-		// 	}
-		// 	$('main section:nth-child('+fsh+') div:nth-child('+fss+')  p:last-child '+sss).attr('disabled', 'disabled');	
-		//     [fsh-1].subheading[fss-2].form.push({'dis':true})
-		// }            
-  //   })
+  // $("#btnGet").click(function(){
+  //   var selectedText=$(".select3").find("option:selected").text();
+  //   var selectedvalue=$(".select3").val();
+  // });
 
-  //   $("#btnGet").click(function(){
-  //   	var selectedText=$(".select3").find("option:selected").text();
-  //   	var selectedvalue=$(".select3").val();
-  //   });
 })
+
+function MYfunction(thise){
+  var q =$(thise).parent().remove()
+}
